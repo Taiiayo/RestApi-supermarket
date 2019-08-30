241dec1e8d20abba56fdb75bf065bfed01d89605
vsh <vsh@adaperio.ru>
2019-08-30 17:18:04 +0300

    initial

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Supermarket.API.Persistence.Contexts;
+
+namespace Supermarket.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IWebHost host = BuildWebHost(args);
+
+            using (IServiceScope scope = host.Services.CreateScope())
+            using (AppDbContext context = scope.ServiceProvider.GetService<AppDbContext>())
+            {
+                context.Database.EnsureCreated();
+            }
+
+            host.Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>()
+            .Build();
+    }
+}
---
+++ Resources/CategoryResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Supermarket.API.Resources
+{
+    public class CategoryResponse
+    {
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; }
+    }
+}
